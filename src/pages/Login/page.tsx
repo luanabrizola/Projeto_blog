098b5c50eb10aa08728e8c2f71020b27c86f8ba4
luanabrizola <130062674+luanabrizola@users.noreply.github.com>
2024-05-02 13:37:24 -0300

    Atualizações

---
+++ src/pages/Login/page.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import style  from './Login.module.css';
+
+export default function Login() {
+  return (
+  
+    <head>
+
+      <div className={style.container_contatos}>
+        <h1>Entre em Contato</h1>
+
+        <form>
+          <label htmlFor="nome">Nome:</label>
+          <input className={style.escrever} type="text" id="nome" name="nome" />
+          <br />
+          <br />
+          <label htmlFor="email">Email:</label>
+          <input className={style.escrever} type="email" id="email" name="email" />
+          <br />
+          <br />
+    
+          <label htmlFor="mensagem">Mensagem:</label>
+          <textarea className={style.escrever} id="mensagem" name="mensagem" placeholder="Digite sua sugestão"></textarea>
+          <br />
+          <br /> 
+          <button className={style.button}>Enviar</button>
+
+
+        </form>
+      </div>
+    </head>
+  );
+}
